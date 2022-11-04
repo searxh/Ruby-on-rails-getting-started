7664e42bd2a7400b4d8fa6efc53d5d78c21bb72f
search <search35453@gmail.com>
2022-11-04 20:56:47 +0700

    added showing single article

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   #declares that GET / articles requests are mapped to the index action of ArticlesController
   get "/articles", to: "articles#index"
 
+  get "/articles/:id", to:"articles#show"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
