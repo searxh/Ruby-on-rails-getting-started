201e1fa7bd5af71fc65d76f0a9968ac9ddd7bab2
search <search35453@gmail.com>
2022-11-04 21:12:42 +0700

    using resources + article_path helper

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root "articles#index"
 
   #declares that GET / articles requests are mapped to the index action of ArticlesController
-  get "/articles", to: "articles#index"
-
-  get "/articles/:id", to:"articles#show"
+  resources :articles
 
   # Defines the root path route ("/")
   # root "articles#index"
