2276df17b8b96f5bb45fe5887893f7d9209a4226
search <search35453@gmail.com>
2022-11-04 19:45:25 +0700

    added articles in routes and articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  #declares that GET / articles requests are mapped to the index action of ArticlesController
+  get "/articles", to: "articles#index"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
